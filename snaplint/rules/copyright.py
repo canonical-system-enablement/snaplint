d9a762c4f796933b440431747335533bd3345541
Scott Sweeny <scott@ssweeny.net>
2016-12-16 13:26:01 -0500

    copyright: Smarter checks for part and staged package copyright files
    Now we check that there is a copyright file for each part in
    and staged package at usr/share/doc/<part/package>/copyright. We are
    assuming that the subdirectory under usr/share/doc will match the part
    name.
    
    Fixes #2

--- snaplint/rules/copyright.py
+++ snaplint/rules/copyright.py
@@ -16,29 +16,66 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import glob
+import os
+import yaml
 
 from snaplint._rule import Rule
 
 
 class CopyrightRule(Rule):
+
     '''Scan for copyright files in usr/share/doc within the snap. System
     Enablement Team policy requires that these files be present'''
 
     def __init__(self, path):
         super().__init__(path)
 
+    def _read_snapcraft_yaml(self):
+        snapcraft = None
+
+        snapcraft_path = os.path.join(self.path, '..', 'snapcraft.yaml')
+        if os.path.exists(snapcraft_path):
+            with open(os.path.join(self.path, '..', 'snapcraft.yaml')) as sc:
+                snapcraft = yaml.load(sc)
+
+        return snapcraft
+
+    def _check_copyrights(self):
+        missing_sps = []
+        missing_parts = []
+        snapcraft = self._read_snapcraft_yaml()
+
+        copyright_path = self.path + 'usr/share/doc/{}/*copyright*'
+
+        for part in snapcraft['parts']:
+            if not glob.glob(copyright_path.format(part)):
+                missing_parts.append(part)
+            if 'stage-packages' in snapcraft['parts'][part]:
+                for pkg in snapcraft['parts'][part]['stage-packages']:
+                    if not glob.glob(copyright_path.format(pkg)):
+                        missing_sps.append(pkg)
+        return (missing_sps, missing_parts)
+
     def scan(self):
-        '''Really dumb check that looks for at least one instance of
-        usr/share/doc/*/*copyright*
-        '''
+        '''Go through the parts and staged packages and make sure there is an
+           entry in usr/share/doc/*copyright* for each one'''
+
         print('Scanning {} for copyright compliance...'.format(self.path),
               end=' ')
 
-        pattern = self.path + 'usr/share/doc/*/*copyright*'
-        if not glob.glob(pattern, recursive=True):
+        missing_sps, missing_parts = self._check_copyrights()
+        if missing_sps or missing_parts:
             print('FAIL')
-            print('No copyright data found')
+            if missing_parts:
+                print('Missing copyright data for the following parts:')
+                for part in missing_parts:
+                    print(part)
+            if missing_sps:
+                print(
+                  'Missing copyright data for the following staged packages:')
+                for pkg in missing_sps:
+                    print(pkg)
             return False
-
-        print('OK')
-        return True
+        else:
+            print('OK')
+            return True
